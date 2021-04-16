941d07a0d2b02a465dc1580b42d7fe7876710a78
ircloud <realtimecompute.luojie@gmail.com>
2021-04-16 16:13:38 +0800

    feat: 使用byDatabricksSparkAvroL读取AVRO文件 & AvroCompression存储AVRO文件

---
+++ 常见大数据项目/spark-learning/src/main/scala/com/bidata/example/avro/ByDatabricksSparkAvro.scala
@@ -0,0 +1,37 @@
+package com.bidata.example.avro
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import java.text.SimpleDateFormat
+import java.util.Date
+/**
+ * read avro by DatabricksSparkAvro
+ */
+object ByDatabricksSparkAvro {
+
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf().setAppName("byDatabricksSparkAvro").setMaster("local")
+    val sc = new SparkContext(conf)
+    // import needed for the .avro method to be added
+    import com.databricks.spark.avro._
+
+    val sqlContext = new SQLContext(sc)
+
+    // The Avro records get converted to Spark types, filtered, and
+    // then written back out as Avro records
+    val df = sqlContext.read
+      .format("com.databricks.spark.avro")
+      .load("file/data/avro/input/episodes.avro")
+    df.show
+
+    val iString = new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date())
+
+    df.filter("doctor > 5").write
+      .format("com.databricks.spark.avro")
+      .save("file/data/avro/output/episodes/avro" + iString)
+    df.filter("doctor > 5").show
+
+  }
+}
