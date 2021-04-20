dbd7223671e7462d7897e7526cb7bec49d005442
ircloud <realtimecompute.luojie@gmail.com>
2021-04-20 11:17:45 +0800

    feat: Spark Structured Streaming

---
+++ 常见大数据项目/spark-learning/src/main/scala/com/bidata/example/structuredstreaming/HandleKafkaJSONExample.scala
@@ -0,0 +1,52 @@
+package com.bidata.example.structuredstreaming
+
+import com.google.gson.Gson
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+
+/**
+ * Created by
+ * Structured 解析Kafka数据
+ */
+object HandleKafkaJSONExample {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName(this.getClass.getSimpleName)
+      .master("local[2]")
+      .getOrCreate()
+
+    val source = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "structured-json-data")
+      .option("maxOffsetsPerTrigger", 1000)
+      .option("startingOffsets", "earliest")
+      .option("failOnDataLoss", "true")
+      .load()
+
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
+
+    val schema = StructType(Seq(
+      StructField("deviceId", StringType),
+      StructField("deviceName", StringType),
+      StructField("deviceValue", DoubleType),
+      StructField("deviceTime", LongType)
+    ))
+
+    val query = source
+      .select(from_json($"value".cast("String"), schema).as("json")).select("json.*")
+      .writeStream
+      .outputMode("update")
+      .format("console")
+      //.option("checkpointLocation", checkpointLocation)
+      .option("truncate", value = false)
+      .start()
+
+    query.awaitTermination()
+  }
+}
