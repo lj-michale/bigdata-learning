d1c6793bfff8b1ae7fe09b8f5378aebc19a790a4
ircloud <realtimecompute.luojie@gmail.com>
2021-06-15 18:12:14 +0800

    feat: FLINK_JIRA_19038

---
+++ 常见大数据项目/flink1.13-learning/src/main/java/com/luoj/task/jira/FLINK_19038.java
@@ -0,0 +1,24 @@
+package com.luoj.task.jira;
+
+
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
+
+/**
+ * @author lj.michale
+ * @description
+ * https://issues.apache.org/jira/browse/FLINK-19038
+ * @date 2021-06-15
+ */
+public class FLINK_19038 {
+    public static void main(String[] args) throws Exception {
+        EnvironmentSettings settings = EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build();
+        TableEnvironment tbEnv = TableEnvironment.create(settings);
+        Table t1 = tbEnv.fromValues("1", "2", "3", "4", "5", "6", "7", "8", "9");
+        Table t2 = t1.orderBy("0").fetch(5);
+        tbEnv.executeSql("create table print(c1 String) with ('connector' = 'print')");
+        tbEnv.insertInto("print",t2);
+        tbEnv.execute("");
+    }
+}
