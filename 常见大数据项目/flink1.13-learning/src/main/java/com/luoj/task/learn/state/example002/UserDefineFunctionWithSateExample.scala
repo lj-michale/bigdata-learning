5100929493be42b6cb1a612991cff3756a1ba147
ircloud <realtimecompute.luojie@gmail.com>
2021-06-25 15:16:03 +0800

    feat: 把计数值分配到不同的并行计数中去

--- 常见大数据项目/flink1.13-learning/src/main/java/com/luoj/task/learn/state/example002/UserDefineFunctionWithSateExample.scala
+++ 常见大数据项目/flink1.13-learning/src/main/java/com/luoj/task/learn/state/example002/UserDefineFunctionWithSateExample.scala
@@ -110,13 +110,11 @@ object UserDefineFunctionWithSateExample {
     private var lastTempState: ValueState[Double] = _
 
     override def open(parameters: Configuration): Unit = {
-      val lastTempDescriptor = new ValueStateDescriptor[Double](
-        "lastTemp", classOf[Double])
+      val lastTempDescriptor = new ValueStateDescriptor[Double]("lastTemp", classOf[Double])
       lastTempState = getRuntimeContext.getState[Double](lastTempDescriptor)
     }
 
-    override def flatMap(reading: SensorReading,
-                         out: Collector[(String, Double, Double)]): Unit = {
+    override def flatMap(reading: SensorReading, out: Collector[(String, Double, Double)]): Unit = {
       val lastTemp = lastTempState.value()
       val tempDiff = (reading.timepreture - lastTemp).abs
 
@@ -126,10 +124,13 @@ object UserDefineFunctionWithSateExample {
 
       this.lastTempState.update(reading.timepreture)
     }
+
   }
 
   // 业务场景为：一个对每一个并行实例的超过阈值的温度的计数程序
   // 使用ListCheckpointed接口来实现操作符的列表状态
+  // 每一个并行实例都计数了本实例有多少温度值超过了设定的阈值。
+  // 例子中使用了操作符状态，并且每一个并行实例都拥有自己的状态变量，这个状态变量将会被检查点操作保存下来，并且可以通过使用ListCheckpointed接口来恢复状态变量。
   class HighTempCounter(val threshold: Double)
     extends RichFlatMapFunction[SensorReading, (Int, Long)]
       with ListCheckpointed[java.lang.Long] {
@@ -140,9 +141,7 @@ object UserDefineFunctionWithSateExample {
     // local count variable
     private var highTempCnt = 0L
 
-    override def flatMap(
-                          in: SensorReading,
-                          out: Collector[(Int, Long)]): Unit = {
+    override def flatMap( in: SensorReading, out: Collector[(Int, Long)]): Unit = {
       if (in.timepreture > threshold) {
         // increment counter if threshold is exceeded
         highTempCnt += 1
@@ -159,10 +158,23 @@ object UserDefineFunctionWithSateExample {
       }
     }
 
-    override def snapshotState(chkpntId: Long, ts: Long): java.util.List[java.lang.Long] = {
-      // snapshot state as list with a single count
-      java.util.Collections.singletonList(highTempCnt)
+//    override def snapshotState(chkpntId: Long, ts: Long): java.util.List[java.lang.Long] = {
+//      // snapshot state as list with a single count
+//      java.util.Collections.singletonList(highTempCnt)
+//    }
+
+    // 我们可以看到操作符的每一个并行实例都暴露了一个状态对象的列表。如果我们增加操作符的并行度，那么一些并行任务将会从0开始计数。
+    // 为了获得更好的状态分区的行为，当HighTempCounter函数扩容时，我们可以按照下面的程序来实现snapshotState()方法，这样就可以把计数值分配到不同的并行计数中去了。
+    override def snapshotState( chkpntId: Long, ts: Long): java.util.List[java.lang.Long] = {
+      // split count into ten partial counts
+      val div = highTempCnt / 10
+      val mod = (highTempCnt % 10).toInt
+      // return count as ten parts
+      (List.fill(mod)(new java.lang.Long(div + 1)) ++
+        List.fill(10 - mod)(new java.lang.Long(div))).asJava
     }
+
+
 
   }
 
