6b068403c20575c49e2534633f54e5d20d9706cb
ircloud <realtimecompute.luojie@gmail.com>
2021-07-26 19:05:13 +0800

    feat: Flink - Flink DataStream ProcessFunction(Key / No Key)

---
+++ 常见大数据项目/flink1.13-learning/src/main/java/com/luoj/task/learn/processfunction/KeyedProcessFunctionExample.java
@@ -0,0 +1,12 @@
+package com.luoj.task.learn.processfunction;
+
+/**
+ * @author lj.michale
+ * @description
+ * @date 2021-07-26
+ */
+public class KeyedProcessFunctionExample {
+
+
+
+}
---
+++ 常见大数据项目/flink1.13-learning/src/main/java/com/luoj/task/learn/processfunction/ProcessFunctionExample.java
@@ -0,0 +1,19 @@
+package com.luoj.task.learn.processfunction;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author lj.michale
+ * @description
+ * @date 2021-07-26
+ */
+@Slf4j
+public class ProcessFunctionExample {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
