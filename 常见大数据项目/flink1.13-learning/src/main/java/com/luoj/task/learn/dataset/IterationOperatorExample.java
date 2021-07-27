96deb571f3f3eeab17f840903c87d14efb54ceee
ircloud <realtimecompute.luojie@gmail.com>
2021-07-27 11:26:42 +0800

    feat: Flink - Passing Parameters to Functions

--- 常见大数据项目/flink1.13-learning/src/main/java/com/luoj/task/learn/dataset/IterationOperatorExample.java
+++ 常见大数据项目/flink1.13-learning/src/main/java/com/luoj/task/learn/dataset/IterationOperatorExample.java
@@ -1,9 +1,12 @@
 package com.luoj.task.learn.dataset;
 
+import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichFilterFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.IterativeDataSet;
+import org.apache.flink.configuration.Configuration;
 
 /**
  * @author lj.michale
@@ -14,7 +17,10 @@ public class IterationOperatorExample {
 
     public static void main(String[] args) throws Exception {
 
+        Configuration conf = new Configuration();
+        conf.setString("mykey","myvalue");
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        env.getConfig().setGlobalJobParameters(conf);
 
         // Create initial IterativeDataSet
         IterativeDataSet<Integer> initial = env.fromElements(0).iterate(100000);
@@ -24,7 +30,6 @@ public class IterationOperatorExample {
             public Integer map(Integer i) throws Exception {
                 double x = Math.random();
                 double y = Math.random();
-
                 return i + ((x * x + y * y < 1) ? 1 : 0);
             }
         });
@@ -38,6 +43,25 @@ public class IterationOperatorExample {
                 return count / (double) 10000 * 4;
             }
         }).print();
+
+        DataSet<Integer> toFilter = env.fromElements(1, 2, 3);
+        Configuration config = new Configuration();
+        config.setInteger("limit", 2);
+        toFilter.filter(new RichFilterFunction<Integer>() {
+            private int limit;
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                limit = parameters.getInteger("limit", 0);
+            }
+
+            @Override
+            public boolean filter(Integer value) throws Exception {
+                return value > limit;
+            }
+        }).withParameters(config);
+
+
 
         env.execute("Iterative Pi Example");
 
