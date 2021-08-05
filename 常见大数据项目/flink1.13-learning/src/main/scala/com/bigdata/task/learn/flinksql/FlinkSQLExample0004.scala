6044aa18a249dbd237d59cf0102966a8a1b7db6a
ircloud <realtimecompute.luojie@gmail.com>
2021-08-05 10:35:24 +0800

    feat: Flink - FlinkSQL

--- 常见大数据项目/flink1.13-learning/src/main/scala/com/bigdata/task/learn/flinksql/FlinkSQLExample0004.scala
+++ 常见大数据项目/flink1.13-learning/src/main/scala/com/bigdata/task/learn/flinksql/FlinkSQLExample0004.scala
@@ -135,11 +135,122 @@ object FlinkSQLExample0004 {
         |LEFT JOIN datagen_dim_source FOR SYSTEM_TIME AS OF datagen_order_source.proctime
         |ON datagen_order_source.userid = datagen_dim_source.id
         |""".stripMargin
-    bsTableEnv.executeSql(sql_analysis).print()
+//    bsTableEnv.executeSql(sql_analysis).print()
 
     /********************************   FlinkSQL的使用   *********************************************/
+    // 维表存储在MySQL中，如下创建维表数据源：
+    val mysql_region_dim_source =
+      """
+        |CREATE TABLE dim_province (
+        |    province_id BIGINT,  -- 省份id
+        |    province_name  VARCHAR, -- 省份名称
+        |    region_name VARCHAR -- 区域名称
+        |) WITH (
+        |    'connector.type' = 'jdbc',
+        |    'connector.url' = 'jdbc:mysql://localhost:3306/bigdata?characterEncoding=utf8&useSSL=false&serverTimezone=UTC&rewriteBatchedStatements=true&allowPublicKeyRetrieval=true',
+        |    'connector.table' = 'dim_province',
+        |    'connector.driver' = 'com.mysql.cj.jdbc.Driver',
+        |    'connector.username' = 'root',
+        |    'connector.password' = 'abc1314520',
+        |    'connector.lookup.cache.max-rows' = '5000',
+        |    'connector.lookup.cache.ttl' = '10min'
+        |)
+        |""".stripMargin
+    bsTableEnv.executeSql(mysql_region_dim_source)
 
+    // 创建kafka数据源表
+    val kafka_order_source =
+      """
+        |CREATE TABLE user_behavior (
+        |    user_id BIGINT, -- 用户id
+        |    item_id BIGINT, -- 商品id
+        |    cat_id BIGINT,  -- 品类id
+        |    action STRING,  -- 用户行为
+        |    province INT,   -- 用户所在的省份
+        |    ts BIGINT,  -- 用户行为发生的时间戳
+        |    proctime as PROCTIME(),   -- 通过计算列产生一个处理时间列
+        |    eventTime AS TO_TIMESTAMP(FROM_UNIXTIME(ts, 'yyyy-MM-dd HH:mm:ss')), -- 事件时间
+        |    WATERMARK FOR eventTime as eventTime - INTERVAL '5' SECOND  -- 在eventTime上定义watermark
+        |) WITH (
+        |    'connector.type' = 'kafka',  -- 使用 kafka connector
+        |    'connector.version' = 'universal',  -- kafka 版本，universal 支持 0.11 以上的版本
+        |    'connector.topic' = 'mzpns',  -- kafka主题
+        |    'connector.startup-mode' = 'earliest-offset',  -- 偏移量，从起始 offset 开始读取
+        |    'connector.properties.group.id' = 'group1', -- 消费者组
+        |    'connector.properties.zookeeper.connect' = 'kms-2:2181,kms-3:2181,kms-4:2181',  -- zookeeper 地址
+        |    'connector.properties.bootstrap.servers' = 'kms-2:9092,kms-3:9092,kms-4:9092',  -- kafka broker 地址
+        |    'format.type' = 'json'  -- 数据源格式为 json
+        |)
+        |""".stripMargin
+    bsTableEnv.executeSql(kafka_order_source)
 
+    // 创建MySQL的结果表，表示区域销量
+    val result_analysis_sink =
+      """
+        |CREATE TABLE region_sales_sink (
+        |    region_name STRING,  -- 区域名称
+        |    buy_cnt BIGINT  -- 销量
+        |) WITH (
+        |    'connector.type' = 'jdbc',
+        |    'connector.url' = 'jdbc:mysql://localhost:3306/bigdata?characterEncoding=utf8&useSSL=false&serverTimezone=UTC&rewriteBatchedStatements=true&allowPublicKeyRetrieval=true',
+        |    'connector.table' = 'top_region', -- MySQL中的待插入数据的表
+        |    'connector.driver' = 'com.mysql.cj.jdbc.Driver',
+        |    'connector.username' = 'root',
+        |    'connector.password' = 'abc1314520',
+        |    'connector.write.flush.interval' = '1s'
+        |)
+        |""".stripMargin
+    bsTableEnv.executeSql(result_analysis_sink)
+
+      // 用户行为数据与省份维表数据join
+//      val join_analysis =
+//        """
+//          |CREATE VIEW user_behavior_detail AS
+//          |SELECT
+//          |  u.user_id,
+//          |  u.item_id,
+//          |  u.cat_id,
+//          |  u.action,
+//          |  p.province_name,
+//          |  p.region_name
+//          |FROM user_behavior AS u LEFT JOIN dim_province FOR SYSTEM_TIME AS OF u.proctime AS p
+//          |ON u.province = p.province_id
+//          |""".stripMargin
+//    bsTableEnv.executeSql(join_analysis)
+
+    // 计算区域的销量，并将计算结果写入MySQL
+//    val data_analysis =
+//      """
+//        |INSERT INTO region_sales_sink
+//        |SELECT
+//        |  region_name,
+//        |  COUNT(*) buy_cnt
+//        |FROM user_behavior_detail
+//        |WHERE action = 'buy'
+//        |GROUP BY region_name
+//        |""".stripMargin
+//    bsTableEnv.executeSql(data_analysis)
+
+//    val data_analysis_print =
+//      """
+//        |INSERT INTO region_sales_sink
+//        |SELECT
+//        |  region_name,
+//        |  COUNT(*) buy_cnt
+//        |FROM (
+//        |	SELECT
+//        |	  u.user_id,
+//        |	  u.item_id,
+//        |	  u.cat_id,
+//        |	  u.action,
+//        |	  p.province_name,
+//        |	  p.region_name
+//        |	FROM user_behavior AS u LEFT JOIN dim_province FOR SYSTEM_TIME AS OF u.proctime AS p
+//        |	ON u.province = p.province_id)
+//        |WHERE action = 'buy'
+//        |GROUP BY region_name
+//        |""".stripMargin
+//    bsTableEnv.executeSql(data_analysis_print).print()
 
 
 
