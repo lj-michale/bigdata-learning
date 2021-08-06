253adcaf7b5217b6d136addde200d5da760021f9
ircloud <realtimecompute.luojie@gmail.com>
2021-08-06 15:57:33 +0800

    feat: Spark3相关特性测试

---
+++ 常见大数据项目/Spark3-Learning/src/main/scala/com/bigdata/runSparkPipeline.scala
@@ -0,0 +1,21 @@
+package com.bigdata
+
+import org.apache.log4j.Logger
+
+/**
+ * @descr runSparkPipeline
+ * @author lj.michale
+ * @date 2021-06
+ */
+object runSparkPipeline {
+
+  val logger = Logger.getLogger(runSparkPipeline.getClass)
+
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+
+
+}
---
+++ 常见大数据项目/Spark3-Learning/src/main/scala/com/bigdata/sql/SparkSQLExample01.scala
@@ -0,0 +1,29 @@
+package com.bigdata.sql
+
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+
+/**
+ * @descr SparkSQLExample01
+ * @author lj.michale
+ * @date 2021-06
+ */
+object SparkSQLExample01 {
+
+  val logger = Logger.getLogger(SparkSQLExample01.getClass)
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("SparkSQLExample01")
+      .master("local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    spark.sql("show databases").show()
+
+    spark.stop()
+
+  }
+
+}
