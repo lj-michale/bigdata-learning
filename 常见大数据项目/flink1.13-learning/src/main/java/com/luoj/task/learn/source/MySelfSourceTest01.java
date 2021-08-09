7a6ceab6641b5bbd64687311824b9bdca16f9b5c
ircloud <realtimecompute.luojie@gmail.com>
2021-08-09 16:19:31 +0800

    feat: Flink - KeyBy

--- 常见大数据项目/flink1.13-learning/src/main/java/com/luoj/task/learn/source/MySelfSourceTest01.java
+++ 常见大数据项目/flink1.13-learning/src/main/java/com/luoj/task/learn/source/MySelfSourceTest01.java
@@ -1,6 +1,5 @@
 package com.luoj.task.learn.source;
 
-
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.TimeCharacteristic;
@@ -11,7 +10,6 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
