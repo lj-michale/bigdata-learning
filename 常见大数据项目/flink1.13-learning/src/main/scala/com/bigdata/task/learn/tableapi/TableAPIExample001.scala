70b4416750d03015fbcd37d5168a7050b79d3afb
ircloud <realtimecompute.luojie@gmail.com>
2021-08-11 15:50:35 +0800

    feat: Flink - Table API 是批处理和流处理的统一的关系型 API。Table API 的查询不需要修改代码就可以采用批输入或流输入来运行。Table API 是 SQL 语言的超集，并且是针对 Apache Flink 专门设计的

--- 常见大数据项目/flink1.13-learning/src/main/scala/com/bigdata/task/learn/tableapi/TableAPIExample001.scala
+++ 常见大数据项目/flink1.13-learning/src/main/scala/com/bigdata/task/learn/tableapi/TableAPIExample001.scala
@@ -4,6 +4,12 @@ import org.apache.flink.api.scala._
 import org.apache.flink.table.api._
 import org.apache.flink.table.api.bridge.scala._
 
+/**
+ * @descr
+ *
+ * https://ci.apache.org/projects/flink/flink-docs-master/zh/docs/dev/table/tableapi/
+ * @author lj.michale
+ */
 object TableAPIExample001 {
 
   def main(args: Array[String]): Unit = {
@@ -28,21 +34,37 @@ object TableAPIExample001 {
     )
 
     val orders = tableEnv.from("GeneratedTable")
-//    val result = orders.groupBy($"a")
-//                .select($"a", $"b".count as "cnt")
-//                .execute()
-//                .print()
+    val result = orders.groupBy($"a")
+                .select($"a", $"b".count as "cnt")
+                .execute()
+                .print()
 
-    val result2: Table = orders
-      .filter($"a".isNotNull && $"b".isNotNull && $"c".isNotNull)
-      .select($"a".lowerCase() as "a", $"b", $"rowtime")
-      .window(Tumble over 1.hour on $"rowtime" as "hourlyWindow")
-      .groupBy($"hourlyWindow", $"a")
-      .select($"a", $"hourlyWindow".end as "hour", $"b".avg as "avgBillingAmount")
+//    val result2: Table = orders
+//      .filter($"a".isNotNull && $"b".isNotNull && $"c".isNotNull)
+//      .select($"a".lowerCase() as "a", $"b", $"rowtime")
+//      .window(Tumble over 1.hour on $"rowtime" as "hourlyWindow")
+//      .groupBy($"hourlyWindow", $"a")
+//      .select($"a", $"hourlyWindow".end as "hour", $"b".avg as "avgBillingAmount")
+//
+//    tableEnv.createTemporaryView("t_result", result2)
+//
+//    tableEnv.executeSql("select * from t_result").print()
 
-    tableEnv.createTemporaryView("t_result", result2)
+    val table2 = tableEnv.fromValues(
+      row(1, "ABC"),
+      row(2L, "ABCDE")
+    )
 
-    tableEnv.executeSql("select * from t_result").print()
+    val table3 = tableEnv.fromValues(
+      DataTypes.ROW(
+        DataTypes.FIELD("id", DataTypes.DECIMAL(10, 2)),
+        DataTypes.FIELD("name", DataTypes.STRING())
+      ),
+      row(1, "ABC"),
+      row(2L, "ABCDE")
+    )
+
+
 
   }
 
