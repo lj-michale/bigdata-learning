3fd4725884cd8da7df0686b8c9437df1c345db73
luojie2 <jie.luo2@ctg.cn>
2022-01-16 23:23:57 +0800

    feat: Flink1.14,欺诈检测

---
+++ 常见大数据项目/flink1.14-learning/src/main/scala/com/bigdata/examples/ex002/FraudDetectionJob.java
@@ -0,0 +1,33 @@
+package com.bigdata.examples.ex002;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.walkthrough.common.sink.AlertSink;
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+import org.apache.flink.walkthrough.common.source.TransactionSource;
+
+
+/**
+ * Skeleton code for the datastream walkthrough
+ */
+public class FraudDetectionJob {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Transaction> transactions = env
+                .addSource(new TransactionSource())
+                .name("transactions");
+
+        DataStream<Alert> alerts = transactions
+                .keyBy(Transaction::getAccountId)
+                .process(new FraudDetector())
+                .name("fraud-detector");
+
+        alerts.addSink(new AlertSink()).name("send-alerts");
+
+        env.execute("Fraud Detection");
+    }
+}
---
+++ 常见大数据项目/flink1.14-learning/src/main/scala/com/bigdata/examples/ex002/FraudDetector.java
@@ -0,0 +1,86 @@
+package com.bigdata.examples.ex002;
+
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+
+/**
+ * Skeleton code for implementing a fraud detector.
+ */
+public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final double SMALL_AMOUNT = 1.00;
+    private static final double LARGE_AMOUNT = 500.00;
+    private static final long ONE_MINUTE = 60 * 1000;
+
+    private transient ValueState<Boolean> flagState;
+    private transient ValueState<Long> timerState;
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        // 1. 引入状态
+        ValueStateDescriptor<Boolean> flagDesc = new ValueStateDescriptor<>("flag", Types.BOOLEAN);
+        flagState = getRuntimeContext().getState(flagDesc);
+
+        // 2. 引入计时器
+        ValueStateDescriptor<Long> timerDescriptor = new ValueStateDescriptor<>("timer-state", Types.LONG);
+        timerState = getRuntimeContext().getState(timerDescriptor);
+
+    }
+
+    @Override
+    public void processElement(
+            Transaction transaction,
+            Context context,
+            Collector<Alert> collector) throws Exception {
+
+        // 1. 获取state
+        Boolean lastTransactionWasSmall = flagState.value();
+        if (lastTransactionWasSmall != null) {
+            if (transaction.getAmount() > LARGE_AMOUNT) {
+
+                Alert alert = new Alert();
+                alert.setId(transaction.getAccountId());
+
+                collector.collect(alert);
+            }
+
+            // 2. clean up our state
+            cleanUp(context);
+        }
+
+        if (transaction.getAmount() < SMALL_AMOUNT) {
+            flagState.update(true);
+
+            // 更新时间计数器
+            long timer = context.timerService().currentProcessingTime() + ONE_MINUTE;
+            context.timerService().registerEventTimeTimer(timer);
+            timerState.update(timer);
+        }
+
+    }
+
+    @Override
+    public void onTimer(long timestamp, OnTimerContext ctx, Collector<Alert> out) throws Exception {
+        // remove flag after 1 minute
+        timerState.clear();
+        flagState.clear();
+    }
+
+    private void cleanUp(Context ctx) throws Exception {
+        // delete timer
+        Long timer = timerState.value();
+        ctx.timerService().deleteEventTimeTimer(timer);
+
+        // clean up  all state
+        timerState.clear();
+        flagState.clear();
+    }
+}
