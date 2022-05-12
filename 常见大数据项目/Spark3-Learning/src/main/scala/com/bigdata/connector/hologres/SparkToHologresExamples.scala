968b0aca20f4fdde4a0d9d276974c6e0b9bfbcc8
luojie2 <jie.luo2@ctg.cn>
2022-05-12 15:49:25 +0800

    feat: spark读取Hologres

---
+++ 常见大数据项目/Spark3-Learning/src/main/scala/com/bigdata/connector/hologres/SparkToHologresExamples.scala
@@ -0,0 +1,52 @@
+package com.bigdata.connector.hologres
+
+import com.alibaba.hologres.spark2.sink.SourceProvider
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+/**
+ * @descri SparkToHologresExamples
+ *
+ * @author lj.michale
+ * @date 2022-05-12
+ */
+object SparkToHologresExamples {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf()
+      .setAppName("DauApp")
+      .setMaster("local[*]")
+    val spark = SparkSession.builder()
+      .config(sparkConf)
+      .getOrCreate()
+
+    val readDf = spark.read
+      // 使用postgresql jdbc driver读取holo
+      .format("jdbc")
+      .option("driver","org.postgresql.Driver")
+      .option("url", "jdbc:postgresql://hgprecn-cn-7pp2nrn1p004-cn-shenzhen.hologres.aliyuncs.com:80/memberpro")
+      .option("dbtable", "hologres_statistic.hg_table_statistic")
+      .option("user", "BASIC$mbrproo")
+      .option("password", "CTG_memberpro@20222")
+      .load()
+
+    readDf.show(200)
+
+//    readDf.write
+//      .format("hologres")
+//      // 阿里云账号的AccessKey ID
+//      .option(SourceProvider.USERNAME, "AccessKey ID")
+//      // 阿里云账号的Accesskey SECRET
+//      .option(SourceProvider.PASSWORD, "Accesskey SECRET")
+//      // Hologres的Ip和Port
+//      .option(SourceProvider.ENDPOINT, "Ip:Port")
+//      .option(SourceProvider.DATABASE, "Database")
+//      .option(SourceProvider.TABLE, "Table")
+//      .save()
+
+    spark.stop()
+
+  }
+
+}
