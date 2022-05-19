c017d5c105797bd2244dacdbdb2e76be7c1a228f
luojie2 <jie.luo2@ctg.cn>
2022-05-19 18:21:05 +0800

    feat: spark3.2学习

---
+++ 常见大数据项目/spark3.2-learning/src/main/java/com/turing/TuringSparkPipeline.java
@@ -0,0 +1,9 @@
+package com.turing;
+/**
+ * @descri
+ *
+ * @author lj.michale
+ * @date 2022-05-19
+ */
+public class TuringSparkPipeline {
+}
---
+++ 常见大数据项目/spark3.2-learning/src/main/scala/com/bigdata/runSparkPipeline.scala
@@ -0,0 +1,16 @@
+package com.bigdata
+
+import org.apache.log4j.Logger
+
+
+object runSparkPipeline {
+
+  val logger = Logger.getLogger(runSparkPipeline.getClass)
+
+  def main(args: Array[String]): Unit = {
+
+
+  }
+
+
+}
